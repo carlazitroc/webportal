edd93ac0585d3fc54df3e09bfabca721a6eb094c
carlazitroc <maricarl.ortiz@gmail.com>
2016-08-12 14:11:02 +0800

    added comment model

---
+++ db/migrate/20160812060710_create_coments.rb
@@ -0,0 +1,11 @@
+class CreateComents < ActiveRecord::Migration
+  def change
+    create_table :coments do |t|
+      t.text :body
+      t.references :user, index: true, foreign_key: true
+      t.references :article, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
